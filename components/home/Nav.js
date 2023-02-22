13aff34fcccb814b92bf78e13c748a91fa84f921
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2023-02-22 19:07:01 -0300

    feat(sigin): add onclick to signin button

--- components/home/Nav.js
+++ components/home/Nav.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
-import React from 'react'
+import { useRouter } from 'next/router';
 
 function Nav() {
+
+  const router = useRouter()
   return (
     <div className='bg-gradient-to-b from-black via-black to-transparent flex flex-row p-5 justify-between absolute w-full top-0 z-50'>
-        <Image alt='logo' src='/logo.png' height={20} width={100} />
-        <button className='bg-red-600 rounded-sm text-white w-16 font-light' >Sign In</button>
+        <Image alt='logo' src='/images/logo.png' height={20} width={100} />
+        <button className='bg-red-600 rounded-sm text-white w-16 font-light' onClick={()=> router.push('login')} >Sign In</button>
     </div>
   )
 }
