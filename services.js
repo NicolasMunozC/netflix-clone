78af5148248af9e3619afb41ff81187808d01947
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2023-02-22 21:20:58 -0300

    feat(serv): create fetch service

--- services.js
+++ services.js
@@ -1,17 +1,6 @@
-import { useState } from "react"
 
 
-export const signIn = () => {
-    window.localStorage.setItem('logged', 'true')
-}
+export default function fetchMovies(){
 
-export const signOut = () => {
-    window.localStorage.removeItem('logged')
-}
-
-export const useLoggin = () => {
-    const [isLogged, setIsLogged] = useState(false)
-    const current = window.localStorage.getItem('logged')
-    if(current) setIsLogged(true)
-    return isLogged
+    
 }
