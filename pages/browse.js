5d973bea3a0b8732bbd10ff1d7c3969260eebd91
Nicolas Muñoz <84532538+NicolasMunozC@users.noreply.github.com>
2023-02-22 21:20:12 -0300

    feat(browse): create browse page

--- pages/browse.js
+++ pages/browse.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import Nav from '@/components/app/Nav'
+import PrivateRoute from '@/components/PrivateRoute'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
-function browse() {
+export default function Browse() {
+    const router = useRouter()
+    const [isLogged, setIsLogged] = useState(false)
 
-    
-  return (
-    <div>browse</div>
-  )
+    useEffect( () => {
+        const currentToken = window.localStorage.getItem('token')
+        if(currentToken) setIsLogged(true)
+        else router.push('/login')
+    },[])
+
+    const signOut = () => {
+        window.localStorage.removeItem('token')
+        setIsLogged(false)
+        router.push('/login')
+    }
+
+
+    return (
+        <PrivateRoute isLogged={isLogged}>
+            <div>
+                <Nav />
+            </div>
+        </PrivateRoute>
+    )
+
+
+
 }
-
-export default browse
